6f89b798efecb28ce30e6a75cf64621ba44de99c
AptimiCt <AptimiCt11@gmail.com>
2019-07-24 22:29:08 +0300

    Initial Commit

---
+++ Vostrikov_HW2.1.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vostrikov_HW2.1.2
+//
+//  Created by Александр Востриков on 24/07/2019.
+//  Copyright © 2019 Александр Востриков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
